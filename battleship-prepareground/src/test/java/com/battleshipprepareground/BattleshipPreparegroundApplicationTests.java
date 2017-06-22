2b0c1257e936039213c2f973c1ef202e67f4c310
Abhishek Mallick <amall3@sapient.com>
2017-06-22 23:17:30 +0530

    Junit

--- battleship-prepareground/src/test/java/com/battleshipprepareground/BattleshipPreparegroundApplicationTests.java
+++ battleship-prepareground/src/test/java/com/battleshipprepareground/BattleshipPreparegroundApplicationTests.java
@@ -5,12 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.battleshipprepareground.BattleshipPreparegroundApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=BattleshipPreparegroundApplication.class)
 public class BattleshipPreparegroundApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
+		
 	}
 
+	
+	
 }
