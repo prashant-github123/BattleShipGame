e02dc3bb024bbb6e5f3395ce5f2a198b58920bb5
Abhishek Mallick <amall3@sapient.com>
2017-06-23 14:51:21 +0530

    Unit Test cases

---
+++ battleship-prepareground/src/test/java/com/test/battleshipprepareground/BattleshipPreparegroundApplicationTests.java
@@ -0,0 +1,22 @@
+package com.test.battleshipprepareground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.battleshipprepareground.BattleshipPreparegroundApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=BattleshipPreparegroundApplication.class)
+public class BattleshipPreparegroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		
+		
+	}
+
+	
+	
+}
