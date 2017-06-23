0bfa7ec54a0566eca7f7ca20262e87a7098f3e2c
Abhishek Mallick <amall3@sapient.com>
2017-06-23 15:23:48 +0530

    Unit Test case

--- battleship-playgame/src/test/java/com/test/battleshipplaygame/BattleshipPlaygameApplicationTests.java
+++ battleship-playgame/src/test/java/com/test/battleshipplaygame/BattleshipPlaygameApplicationTests.java
@@ -1,16 +1,22 @@
-package com.battleshipplaygame;
+package com.test.battleshipplaygame;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.battleshipplaygame.BattleshipPlaygameApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=BattleshipPlaygameApplication.class)
 public class BattleshipPlaygameApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
+		
 	}
 
+	
+	
 }
---
+++ battleship-playgame/src/test/java/com/test/battleshipplaygame/PlayBattleshipGameControllerUnitTest.java
@@ -0,0 +1,87 @@
+package com.test.battleshipplaygame;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.battleshipplaygame.BattleshipPlaygameApplication;
+import com.battleshipplaygame.controller.PlayBattleShipGameController;
+import com.battleshipplaygame.domain.BattleShipGame;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {BattleshipPlaygameApplication.class})
+public class PlayBattleshipGameControllerUnitTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private BattleShipGame game;
+    
+    @InjectMocks
+    private PlayBattleShipGameController playgameController;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(playgameController)
+                .build();
+    }
+
+    @Test
+    public void testCheckTurnP1UnitService() throws Exception {
+        
+    		
+    	
+    		//Check service when Player hit the enemy ship.
+    		mockMvc.perform(
+			        post("/rest/api/checkTurnStatus")
+			        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+			        .content("{"
+								+ "\"gameId\": \"111\","
+								+ "\"playerId\": \"p1\","
+								+ "\"playerName\": \"Prashant\","
+								+ "\"hitOrMiss\":\"hit\","
+								+ "\"gameOver\": false,"
+								+ "\"checkTurnStatusOnly\":false,"
+								+ "\"hitCoordinate\":\"20\""
+								+ "}"))			                
+				.andExpect(status().isOk());
+    		
+    }
+    
+    @Test
+    public void testCheckTurnP2UnitService() throws Exception {
+        
+    		//Check service when Player check for his turn
+    		mockMvc.perform(
+			        post("/rest/api/checkTurnStatus")
+			        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+			        .content("{"
+								+ "\"gameId\": \"111\","
+								+ "\"playerId\": \"p2\","
+								+ "\"playerName\": \"Abhishek\","
+								+ "\"hitOrMiss\":\"\","
+								+ "\"gameOver\": false,"
+								+ "\"checkTurnStatusOnly\":true,"
+								+ "\"hitCoordinate\":\"\""
+								+ "}"))			                
+				.andExpect(status().isOk());
+    		
+    }
+
+    
+}
